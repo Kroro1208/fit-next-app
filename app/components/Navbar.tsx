08a3489847f04950e529b0173dfa5b08c5b4b1a1
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-07-09 08:00:25 +0900

    theme切り替え実装した

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image"
 import Link from "next/link"
-import logo from "../../public/2.png"
+import logo from "../../public/fitness.gif"
+import { ThemeToggle } from "./ThemeToggle"
 
 const Navbar = () => {
     return (
         <nav className="h-[120px] w-full flex items-center justify-between border-b px-5 lg:px-14">
             <Link href="/" className="flex items-center gap-x-3"> 
-                <Image src={logo} alt="desktop-text" className="h-48 p-1 mt-3 w-fit hidden lg:block" />
+                <Image src={logo} alt="desktop-text" className="h-24 p-1 mt-3 w-fit hidden lg:block" />
             </Link>
+            <ThemeToggle />
         </nav>
     )
 }
