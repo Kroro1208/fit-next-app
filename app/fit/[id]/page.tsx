f9a2a4300f65a096bcce15ec43562e0ed1dea1ad
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-08-02 11:35:37 +0900

    自分のコミュニティないの投稿表示部分を実装

--- app/fit/[id]/page.tsx
+++ app/fit/[id]/page.tsx
@@ -1,37 +1,93 @@
-import SubDesciptionForm from '@/app/components/SubDesciptionForm'
+import CreatePostCard from '@/app/components/CreatePostCard';
+import Pagination from '@/app/components/Pagination';
+import PostCard from '@/app/components/PostCard';
+import SubDescriptionForm from '@/app/components/SubDescriptionForm'
 import prisma from '@/app/lib/db'
 import { Button } from '@/components/ui/button';
 import { Card } from '@/components/ui/card'
 import { Separator } from '@/components/ui/separator';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { AlarmClock } from 'lucide-react';
+import { unstable_noStore } from 'next/cache';
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-async function getData(name: string) {
-    const data = await prisma.community.findUnique({
-        where: {
-            name: name,
-        },
-        select: {
-            name: true,
-            createdAt: true,
-            description: true,
-            userId: true
-        }
-    });
-    return data;
+async function getData(name: string, searchParam: string) {
+    unstable_noStore();
+    const [count, data] = await prisma.$transaction([
+        prisma.post.count({
+            where: {
+                subName: name
+            }
+        }),
+        prisma.community.findUnique({
+            where: {
+                name: name,
+            },
+            select: {
+                name: true,
+                createdAt: true,
+                description: true,
+                userId: true,
+                posts: {
+                    take: 10,
+                    skip: searchParam ? (Number(searchParam) - 1) * 10 : 0,
+                    select: {
+                        title: true,
+                        imageString: true,
+                        id: true,
+                        textContent: true,
+                        Vote: {
+                            select: {
+                                userId: true,
+                                voteType: true,
+                            },
+                        },
+                        User: {
+                            select: {
+                                userName: true
+                            }
+                        }
+                    }
+                }
+            }
+        })
+    ]);
+    return { data, count };
 }
 
-const CommunityRoute = async ({ params }: { params: {id: string}}) => {
-    const data = await getData(params.id);
+const CommunityRoute = async ({ 
+    params,
+    searchParams }:
+    {
+        params: {id: string},
+        searchParams: {page: string}
+    }) => {
+    const {count, data} = await getData(params.id, searchParams.page);
     const { getUser } = getKindeServerSession();
     const user = await getUser();
     return (
-        <div className='max-w-[1000px] mx-auto flex gap-x-10 mt-4'>
+        <div className='max-w-[1000px] mx-auto flex gap-x-10 mt-10'>
             <div className='w-[65%] flex flex-col gap-y-5'>
-                <h1>投稿セクションです</h1>
+                <CreatePostCard />
+                {data?.posts.map((post)=>(
+                    <PostCard
+                        key={post.id}
+                        id={post.id} 
+                        imageString={post.imageString}
+                        subName={data.name}
+                        title={post.title}
+                        userName={post.User?.userName as string}
+                        jsonContent={post.textContent}
+                        voteCount={post.Vote.reduce((acc, vote) => {
+                            if(vote.voteType === "UP") return acc + 1;
+                            if(vote.voteType === "DOWN") return acc - 1;
+                            return acc;
+                            },0 )}
+                    />
+                ))}
+                <Pagination totalPages={Math.ceil(count / 10)} />
             </div>
             <div className='w-[35%]'>
                 <Card>
@@ -48,7 +104,7 @@ const CommunityRoute = async ({ params }: { params: {id: string}}) => {
                             </Link>
                         </div>
                         {user?.id === data?.userId ? (
-                            <SubDesciptionForm description={data?.description} subName={params.id}/>
+                            <SubDescriptionForm description={data?.description} subName={params.id}/>
                         ): (
                             <p className='text-sm font-normal text-secondary-foreground mt-2'>
                                 {data?.description}
