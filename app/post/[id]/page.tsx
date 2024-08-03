3c7d18c6b6c10f2ef128c0d5e5df3a9e57c727c5
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-08-03 11:34:46 +0900

    コミュニティページに投稿内容表示させて、コメントモデル作成

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -3,10 +3,14 @@ import { Card } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
-import SubDescriptionForm from '@/app/components/SubDescriptionForm';
-import { AlarmClock } from "lucide-react";
+import { AlarmClock, MessageCircle } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
+import { handleVote } from "@/app/actions";
+import UpVoteButton from "@/app/components/UpVoteButton";
+import DownVoteButton from "@/app/components/DownVoteButton";
+import RenderJson from "@/app/components/RenderJson";
+import CopyLink from "@/app/components/CopyLink";
 
 async function getData(id: string) {
     const data = await prisma.post.findUnique({
@@ -19,6 +23,12 @@ async function getData(id: string) {
             imageString: true,
             textContent: true,
             subName: true,
+            id: true,
+            Vote: {
+                select: {
+                    voteType: true
+                }
+            },
             Community: {
                 select: {
                     name: true,
@@ -45,7 +55,41 @@ const PostPage = async ({ params }: { params: {id: string} }) => {
     return (
         <div className="max-w-[1200px] mx-auto flex gap-x-10 mt-4 mb-10">
             <div className="w-[70%] flex flex-col gap-y-5">
-                <h1>投稿ページです</h1>
+                <Card className="flex p-2">
+                    <div className="flex flex-col items-center gap-y-2 p-2">
+                        <form action={handleVote}>
+                            <input type="hidden" name='voteDirection' value="UP"/>
+                            <input type="hidden" name='postId' value={data.id}/>
+                            <UpVoteButton />
+                        </form>
+                        {data.Vote.reduce((acc, vote) => {
+                            if(vote.voteType === 'UP') return acc + 1;
+                            if(vote.voteType === 'DOWN') return acc - 1;
+                            return acc;
+                            }, 0)}
+                        <form action={handleVote}>
+                            <input type="hidden" name='voteDirection' value="DOWN"/>
+                            <input type="hidden" name='postId' value={data.id}/>
+                            <DownVoteButton />
+                        </form>
+                    </div>
+                    <div className="p-2 w-full">
+                        <p className="text-sm text-muted-foreground">投稿者: {data.User?.userName}</p>
+                        <h1 className="font-medium mt-1 text-lg">{data.title}</h1>
+                        {data.imageString && (
+                            <Image src={data.imageString} alt="UserImage" width={500} height={400}
+                            className="w-full h-auto object-contain mt-2"/>
+                        )}
+                        {data.textContent && (<RenderJson data={data.textContent}/>)}
+                        <div className="flex gap-x-5 items-center mt-3">
+                            <div className="flex items-center gap-x-1">
+                                <MessageCircle className="h-5 w-5 text-muted-foreground"/>
+                                <p className="text-muted-foreground font-medium text-sm">15件のコメント</p>
+                            </div>
+                            <CopyLink id={params.id}/>
+                        </div>
+                    </div>
+                </Card>
             </div>
             <div className="w-[30%]">
             <Card>
