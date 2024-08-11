53477271a034ca5aa94bfb744fc71767b222ea67
Kroro1208 <yamagata_7580@yahoo.co.jp>
2024-08-11 15:54:53 +0900

    logo変更した

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
-import logo from "../../public/brain.png"
+import logo from "../../public/banner2.png"
 import { ThemeToggle } from "./ThemeToggle"
 import { Button } from "@/components/ui/button"
 import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
@@ -25,9 +25,9 @@ const Navbar = async () => {
     }
 
     return (
-        <nav className="h-[120px] w-full flex items-center justify-between border-b px-5 lg:px-14">
+        <nav className="h-[120px] w-full flex items-center justify-between border-b px-5 lg:px-14 bg-green-600">
             <Link href="/" className="flex items-center gap-x-3"> 
-                <Image src={logo} alt="desktop-text" className="h-24 p-1 mt-3 w-fit hidden lg:block" />
+                <Image src={logo} alt="desktop-text" className="h-28 p-1 w-[550px] hidden lg:block rounded-xl" />
             </Link>
             <div className="flex items-center gap-x-4">
                 {
